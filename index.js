31545ff2a2ef677f08ce49e5a973b3ca60e2fa93
enmanuelKantt <enmanuel.a.o.l@gmail.com>
2023-08-29 22:34:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Requirements
+// • Crear un programa que permita hacer el CRUD completo de los datos. El id será el primer
+
+// argumento para acceder a las propiedades de cada anime.
+// • Se deberá poder listar todos los datos del archivo y, además, leer los datos de un anime
+
+// especifico, accediendo por su id y / o por su nombre.
+// • Realizar un test para poder probar la respuesta del servidor que fue creado.
+
+// 147
